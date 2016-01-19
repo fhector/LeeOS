c5049771b79ba15701608ffd5bce053881adcb70
hector <f_hector@hotmail.com>
2016-01-19 11:56:36 +0800

    Version_6_Simple_Interrupt
    Version_6_Simple_Interrupt

--- boot.c
+++ boot.c
@@ -26,6 +26,28 @@ static init_func init[]=
     0,
 };
 
+void timer_init(void)
+{
+#define TIMER_BASE  (0xd1000000)
+#define TCFG0   ((volatile unsigned int *)(TIMER_BASE+0x0))
+#define TCFG1   ((volatile unsigned int *)(TIMER_BASE+0x4))
+#define TCON    ((volatile unsigned int *)(TIMER_BASE+0x8))
+#define TCONB4  ((volatile unsigned int *)(TIMER_BASE+0x3c))
+    *TCFG0|=0x800;
+    *TCON&=(~(7<<20));
+    *TCON|=(1<<22);
+    *TCON|=(1<<21);
+
+    *TCONB4=10000;
+
+    *TCON|=(1<<20);
+    *TCON&=~(1<<21);
+
+    umask_int(14);
+    enable_irq();
+}
+
+
 
 void plat_boot(void)
 {
@@ -38,5 +60,6 @@ void plat_boot(void)
     start_mmu();
     test_mmu();
     test_printk();
+    timer_init();
     while(1);
 }
